558fee289759af5866bc170acc48f05f8f1dd6c9
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2025-11-17 21:10:31 +0100

    utils added (to be checked)

--- Parser/utils.c
+++ Parser/utils.c
@@ -16,3 +16,33 @@ int	is_whitespace(char c)
 {
 	return (c == ' ' || c == '\t' || c == '\n');
 }
+
+int	is_metacharacter(char c)
+{
+	return (c == '|' || c == '<' || c == '>' || c == ';');
+}
+
+int	is_redirection_token(t_token_type type)
+{
+	return (type == TK_REDIR_IN || type == TK_REDIR_OUT ||
+			type == TK_REDIR_APP || type == TK_HEREDOC);
+}
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*result;
+
+	result = ft_strjoin(s1, s2);
+	if (s1)
+		free(s1);
+	if (s2)
+		free(s2);
+	return (result);
+}
+
+void	print_error(char *msg)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(msg, 2);
+	ft_putstr_fd("\n", 2);
+}
