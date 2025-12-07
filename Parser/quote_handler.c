7a91ab4b973140b3cac54b7a593fe807fd903a4f
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2025-12-07 13:11:02 +0100

    norminette ok and updated

--- Parser/quote_handler.c
+++ Parser/quote_handler.c
@@ -6,16 +6,18 @@
 */
 void	process_quotes_and_expansion(t_shell *shell)
 {
-	t_token	*current_token;
-	char	*processed_value;
+	t_token			*current_token;
+	char			*processed_value;
+	t_expand_args	expand_args;
 
+	expand_args.exit_status = shell->exit_status;
 	current_token = shell->tokens;
 	while (current_token != NULL)
 	{
 		if (current_token->type == TK_WORD && current_token->value != NULL)
 		{
-			processed_value = remove_quotes_and_expand(current_token->value, 
-									shell->env, shell->exit_status);
+			processed_value = remove_quotes_and_expand(current_token->value,
+					&expand_args);
 			if (processed_value != NULL)
 			{
 				free(current_token->value);
@@ -30,198 +32,67 @@ void	process_quotes_and_expansion(t_shell *shell)
 ** Main function to remove quotes and expand variables
 ** Handles three contexts: single quotes, double quotes, and unquoted
 */
-char	*remove_quotes_and_expand(char *str, char **env, int exit_status)
+char	*remove_quotes_and_expand(char *str, t_expand_args *expand_args)
 {
-	char	*result;
-	int		i;
+	int	i;
 
-	result = ft_strdup("");
+	expand_args->result = ft_strdup("");
 	i = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] == '\'')
-			i = handle_single_quotes(str, i, &result);
+			i = handle_single_quotes(str, i, expand_args);
 		else if (str[i] == '\"')
-			i = handle_double_quotes(str, i, &result, env, exit_status);
+			i = handle_double_quotes(str, i, expand_args);
 		else if (str[i] == '$')
-			i = expand_variable(str, i, &result, env, exit_status);
+			i = expand_variable(str, i, expand_args);
 		else
 		{
-			result = ft_strjoin_free(result, ft_substr(str, i, 1));
+			expand_args->result = ft_strjoin_free(expand_args->result,
+					ft_substr(str, i, 1));
 			i++;
 		}
 	}
-	return (result);
+	return (expand_args->result);
 }
 
 /*
 ** Handle single quotes - no expansion, literal content
 */
-int	handle_single_quotes(char *str, int i, char **result)
+int	handle_single_quotes(char *str, int i, t_expand_args *expand_args)
 {
-	i++; // Skip opening quote
-	
-	// Copy everything until closing quote
+	i++;
 	while (str[i] != '\0' && str[i] != '\'')
 	{
-		*result = ft_strjoin_free(*result, ft_substr(str, i, 1));
+		expand_args->result = ft_strjoin_free(expand_args->result,
+				ft_substr(str, i, 1));
 		i++;
 	}
-	
-	// Skip closing quote if found
 	if (str[i] == '\'')
 		i++;
-	
 	return (i);
 }
 
 /*
 ** Handle double quotes - expand variables but keep other characters literal
 */
-int	handle_double_quotes(char *str, int i, char **result, 
-						char **env, int exit_status)
+int	handle_double_quotes(char *str, int i, t_expand_args *expand_args)
 {
-	i++; // Skip opening quote
-	
+	i++;
 	while (str[i] != '\0' && str[i] != '\"')
 	{
 		if (str[i] == '$')
-			i = expand_variable_in_dquotes(str, i, result, env, exit_status);
+			i = expand_variable(str, i, expand_args);
 		else
 		{
-			*result = ft_strjoin_free(*result, ft_substr(str, i, 1));
+			expand_args->result = ft_strjoin_free(expand_args->result,
+					ft_substr(str, i, 1));
 			i++;
 		}
 	}
-	
-	// Skip closing quote if found
 	if (str[i] == '\"')
 		i++;
-	
 	return (i);
-}
-
-/*
-** Expand variable in double quotes context
-** Returns new position in string
-*/
-int	expand_variable_in_dquotes(char *str, int i, char **result, 
-							   char **env, int exit_status)
-{
-	char	*var_name;
-	char	*var_value;
-
-	i++; // Skip $
-	
-	// Handle $? (exit status)
-	if (str[i] == '?')
-	{
-		var_value = ft_itoa(exit_status);
-		*result = ft_strjoin_free(*result, var_value);
-		return (i + 1);
-	}
-	// Handle environment variables
-	else if (ft_isalpha(str[i]) || str[i] == '_')
-	{
-		var_name = extract_var_name(str, i);
-		var_value = get_env_value(var_name, env);
-		if (var_value != NULL)
-		{
-			*result = ft_strjoin_free(*result, ft_strdup(var_value));
-			free(var_value);
-		}
-		free(var_name);
-		return (i + ft_strlen(var_name));
-	}
-	else
-	{
-		// Not a valid variable, keep $ as literal
-		*result = ft_strjoin_free(*result, ft_substr(str, i - 1, 1));
-		return (i);
-	}
-}
-
-/*
-** Expand variable in unquoted context
-** Returns new position in string
-*/
-int	expand_variable(char *str, int i, char **result, 
-					char **env, int exit_status)
-{
-	char	*var_name;
-	char	*var_value;
-
-	i++; // Skip $
-	
-	// Handle $? (exit status)
-	if (str[i] == '?')
-	{
-		var_value = ft_itoa(exit_status);
-		*result = ft_strjoin_free(*result, var_value);
-		return (i + 1);
-	}
-	// Handle environment variables
-	else if (ft_isalpha(str[i]) || str[i] == '_')
-	{
-		var_name = extract_var_name(str, i);
-		var_value = get_env_value(var_name, env);
-		if (var_value != NULL)
-		{
-			*result = ft_strjoin_free(*result, ft_strdup(var_value));
-			free(var_value);
-		}
-		free(var_name);
-		return (i + ft_strlen(var_name));
-	}
-	else
-	{
-		// Not a valid variable, keep $ as literal
-		*result = ft_strjoin_free(*result, ft_substr(str, i - 1, 1));
-		return (i);
-	}
-}
-
-/*
-** Extract variable name from string (alphanumeric + underscore)
-*/
-char	*extract_var_name(char *str, int i)
-{
-	int		start;
-	int		len;
-
-	start = i;
-	len = 0;
-	
-	while (str[i] != '\0' && (ft_isalnum(str[i]) || str[i] == '_'))
-	{
-		i++;
-		len++;
-	}
-	
-	return (ft_substr(str, start, len));
-}
-
-/*
-** Get environment variable value
-*/
-char	*get_env_value(char *var_name, char **env)
-{
-	int		i;
-	int		len;
-	char	*env_var;
-
-	i = 0;
-	len = ft_strlen(var_name);
-	while (env[i] != NULL)
-	{
-		if (ft_strncmp(env[i], var_name, len) == 0 && env[i][len] == '=')
-		{
-			env_var = ft_strdup(env[i] + len + 1);
-			return (env_var);
-		}
-		i++;
-	}
-	return (NULL);
 }
 
 /*
@@ -238,7 +109,6 @@ char	*handle_heredoc_delimiter(char *str)
 	{
 		if (str[i] == '\'' || str[i] == '\"')
 		{
-			// For heredoc, quotes are part of the delimiter
 			result = ft_strjoin_free(result, ft_substr(str, i, 1));
 			i++;
 		}
